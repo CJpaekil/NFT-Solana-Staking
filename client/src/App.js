ae4756d36a6cd8ee0f09ea076aceab633b28d5e4
firstworld <firstworld1112@outlook.com>
2022-03-07 02:39:45 +0900

    all updated

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import Header from './component/Header/Header';
+import StakingPage from './component/StakingPage/StakingPage';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Header/>
+      <StakingPage/> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/component/Header/Header.js
@@ -0,0 +1,18 @@
+import { Styles } from './Style';
+const Header = ({ 
+}) => {
+    return (
+        <Styles>
+            <header className="">
+                <div className="container">
+                    <div className="position-relative">
+                        <div>sdfsd</div>
+                        <div>xcvcxvxcv</div>
+                    </div>
+                </div>
+            </header>
+        </Styles>
+    )
+}
+
+export default Header;
